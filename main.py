4271e99141e9c01e4eaa8734ec02f0f4e5e5cdc9
Nicolas Derouineau <nicolas.derouineau@ymagis.com>
2019-04-17 16:05:28 +0200

    handle input arguments

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 import sys, os, fnmatch,  getopt
 
 
-def usage(prog):
-    print("%s is a simple program to convert a set of dpx to ffv1 codec" % prog)
+def usage():
+    print("%s is a simple program to convert a set of dpx to ffv1 codec" % sys.argv[0])
     print("Expected usage:")
-    print("%s path=/home/user/dpxdirectory/ output=/home/user/ffv1out.mkv"% prog)
+    print("%s --input=/home/user/dpxdirectory/ --output=/home/user/ffv1out.mkv "% sys.argv[0])
+
+def print_error(error_message):
+    print(error_message)
+    usage()
+    sys.exit()
 
 def parse_directory(path):
     for entry in listOfFiles:  
@@ -17,9 +22,7 @@ def main():
         opts, args = getopt.getopt(sys.argv[1:], "hop:v", ["help", "output=", "input="])
     except getopt.GetoptError as err:
         # print help information and exit:
-        print(err)  # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
+        print_error(err)  # will print something like "option -a not recognized"
     output = None
     input = None
     verbose = False
@@ -27,27 +30,25 @@ def main():
         if o == "-v":
             verbose = True
         elif o in ("-h", "--help"):
-            usage(sys.argv[0])
+            usage()
             sys.exit()
         elif o in ("-o", "--output"):
             output = a
             print("output is %s " % output)
         elif o in ("-i", "--input"):
             input = a
-            print("path is %s " % path)
+            print("path is %s " % input)
+            if(os.path.isdir(input) == False):
+                print_error("The input path is not a directory")
         else:
             assert False, "unhandled option"
     
     if input is None:
-        print("No input was given")
-        usage(sys.argv[0])
-        sys.exit()
+        print_error("No input was given")
     if output is None:
-        print("No Output was given")
-        usage(sys.argv[0])
-        sys.exit()
+        print_error("No Output was given")
     
-    num_scan, offset = parse_directory()
+    #num_scan, offset = parse_directory(path)
     #encode_dpx_scans(path, output, num_scan, offset, fps)
 
 if __name__ == "__main__":
