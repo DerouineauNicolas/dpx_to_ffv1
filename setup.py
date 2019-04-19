872c8e1dfaa4cc9e92a8b93247b2bbe31c3c58aa
Nicolas Derouineau <nicolas.derouineau@ymagis.com>
2019-04-19 16:11:45 +0200

    Add license and update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,18 @@ with open("README.md", "r") as fh:
 
 setup(
     name='dpx2ffv1',
+    description='dpx2ffv1 is a simple module to convert a set of dpx to ffv1 codec',
+    version='0.1.0',
+    url='https://github.com/DerouineauNicolas/dpx_to_ffv1',
+    author='N. DEROUINEAU',
+    author_email='nicolas.derouineau@ymagis.com',
+    license='WTFPL',
     package_dir={'dpx2ffv1': 'dpx2ffv1'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
     packages=find_packages(),
-    entry_points={ 'console_scripts': ['Package = dpx2ffv1.__main__:main' ] })
+    entry_points={'console_scripts': ['Package = dpx2ffv1.__main__:main']})
